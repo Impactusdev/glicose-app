4aa12f0c961359767532e23275f41c72bd60dd9e
Pinelli <ricardosilvapinelli@gmail.com>
2023-01-07 23:58:42 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Aplicativo Glicose`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
